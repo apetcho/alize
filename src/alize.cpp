64bf68c300523c6b3faede3755a95c2389d007ab
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-05-22 11:42:39 +0100

    Implement Object::operator/(const Object&, const Object&)

--- src/alize.cpp
+++ src/alize.cpp
@@ -804,8 +804,41 @@ Object operator*(const Object& lhs, const Object& rhs){
     return result;
 }
 
+// -*-
+Object operator/(const Object& lhs, const Object& rhs){
+    Object result{};
+    if(!(lhs.type()==rhs.type())){
+        std::stringstream stream;
+        stream << "type mismatch. `/` is only applicable between `numbers`";
+        throw Error(Error::Kind::TypeError, stream.str());
+    }
+    if(lhs.is_number()){
+        if(lhs.is_integer() && rhs.is_integer()){
+            auto x = std::get<i64>(lhs.m_value);
+            auto y = std::get<i64>(rhs.m_value);
+            if(y==0){
+                throw Error(Error::Kind::ValueError, "division by zero");
+            }
+            result = Object((x / y));
+        }else{
+            auto x = static_cast<f64>(Object(lhs));
+            auto y = static_cast<f64>(Object(rhs));
+            if(Alize::almostEqual(y, 0.0)){
+                throw Error(Error::Kind::ValueError, "division by zero");
+            }
+            result = Object((x / y));
+        }
+    }else{
+        std::stringstream stream;
+        stream << "type mismatch. `/` is only applicable between `numbers`";
+        throw Error(Error::Kind::TypeError, stream.str());
+    }
+
+    return result;
+}
+
+
 /*
-Object operator/(const Object& lhs, const Object& rhs){}
 Object operator%(const Object& lhs, const Object& rhs){}
 Object operator||(const Object& lhs, const Object& rhs){}
 Object operator&&(const Object& lhs, const Object& rhs){}
