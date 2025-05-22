11b796066cdd170e87cc84611123e1f2bb2d1124
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-05-22 11:35:57 +0100

    Implement Object::operator+(const Object&, const Object&)

--- src/alize.cpp
+++ src/alize.cpp
@@ -709,9 +709,45 @@ Object& Object::operator~(){
     return *this;
 }
 
-/*
 // -*- binary-operator: {-, +, *, /, %, and, or,} -*-
-Object operator+(const Object& lhs, const Object& rhs){}
+Object operator+(const Object& lhs, const Object& rhs){
+    Object result{};
+    if(!(lhs.type()==rhs.type())){
+        std::stringstream stream;
+        stream << "type mismatch. `+` is only applicable between `numbers`, ";
+        stream << " or `strings` or `lists`";
+        throw Error(Error::Kind::TypeError, stream.str());
+    }
+    if(lhs.is_number()){
+        if(lhs.is_integer() && rhs.is_integer()){
+            auto x = std::get<i64>(lhs.m_value);
+            auto y = std::get<i64>(rhs.m_value);
+            result = Object((x+y));
+        }else{
+            auto x = static_cast<f64>(Object(lhs));
+            auto y = static_cast<f64>(Object(rhs));
+            result = Object((x + y));
+        }
+    }else if(lhs.is_string()){
+        auto xstr = std::get<Str>(lhs.m_value);
+        auto ystr = std::get<Str>(rhs.m_value);
+        auto str = xstr + ystr;
+        result = Object(str);
+    }else if(lhs.is_list()){
+        auto xs = std::get<ArrayList>(lhs.m_value);
+        auto ys = std::get<ArrayList>(rhs.m_value);
+        for(auto yptr = ys.cbegin(); yptr != ys.cend(); yptr++){
+            xs.emplace_back(*yptr);
+        }
+        result = Object(xs);
+    }
+
+    return result;
+}
+
+
+
+/*
 Object operator-(const Object& lhs, const Object& rhs){}
 Object operator*(const Object& lhs, const Object& rhs){}
 Object operator/(const Object& lhs, const Object& rhs){}
